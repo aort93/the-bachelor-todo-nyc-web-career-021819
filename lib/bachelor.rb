97fd13cbb2b228f0831ccd339c78bc04c62ec9a9
Alonso Ortega <alonsojortega@gmail.com>
2019-02-07 19:50:53 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -32,7 +32,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  
+  data.each do |season, contestants|
+    contestants.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        return contestant_info['occupation']
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
